a31e8e42b1e067b9f4e12b6369b4303ac29234c8
Morten Lunde <morten.lunde@live.no>
2024-01-24 14:33:12 +0100

    init

---
+++ Pets/Pet.cs
@@ -0,0 +1,21 @@
+namespace Pets;
+
+internal abstract class Pet(string name, string description, string trick, string sound)
+{
+    public string Name { get; set; } = name;
+    public string Description { get; set; } = description;
+    public string Trick { get; set; } = trick;
+    public string Sound { get; set; } = sound;
+}
+
+internal class Dog(string name, string breed) : Pet(name, "Dog", "Lie down", "Woff")
+{
+    public string Breed { get; set; } = breed;
+}
+
+internal class Cat(string name, string breed) : Pet(name, "Cat", "Cuddle", "Purr")
+{
+    public string Breed { get; set; } = breed;
+}
+
+internal class Fish(string name) : Pet(name, "Fish", "Splash", "Gulp");
---
+++ Pets/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Pets
+{
+    internal static class Program
+    {
+        internal static void Main()
+        {
+            Pet animal1 = new Dog("Pluto", "German Shepard");
+            Pet animal2 = new Cat("Garfield", "Bengal cat");
+            Pet animal3 = new Fish("Magicarp");
+            Pet animal4 = new Dog("Lassie", "Collie");
+            Pet animal5 = new Cat("Puss in Boots", "British shorthair");
+
+            List<Pet> pets =
+            [
+                animal1,
+                animal2,
+                animal3,
+                animal4,
+                animal5
+            ];
+
+            foreach (Pet p in pets)
+            {
+                switch (p)
+                {
+                    case Dog dog:
+                        Console.WriteLine($"{dog.Name} - {dog.Description} ({dog.Breed}): {dog.Trick}, Sound: {dog.Sound}");
+                        break;
+                    case Cat cat:
+                        Console.WriteLine($"{cat.Name} - {cat.Description} ({cat.Breed}): {cat.Trick}, Sound: {cat.Sound}");
+                        break;
+                    default:
+                        Console.WriteLine($"{p.Name} - {p.Description}: {p.Trick}, Sound: {p.Sound}");
+                        break;
+                }
+            }
+        }
+    }
+}

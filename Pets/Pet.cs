923d6ea736d33908521677792df8c7e45a996020
Morten Lunde <morten.lunde@live.no>
2024-01-25 10:24:16 +0100

    fix(Pet.cs) updated namespace to brackets around code

--- Pets/Pet.cs
+++ Pets/Pet.cs
@@ -1,21 +1,22 @@
-namespace Pets;
-
-internal abstract class Pet(string name, string description, string trick, string sound)
+namespace Pets
 {
-    public string Name { get; set; } = name;
-    public string Description { get; set; } = description;
-    public string Trick { get; set; } = trick;
-    public string Sound { get; set; } = sound;
-}
+    internal abstract class Pet(string name, string description, string trick, string sound)
+    {
+        public string Name { get; set; } = name;
+        public string Description { get; set; } = description;
+        public string Trick { get; set; } = trick;
+        public string Sound { get; set; } = sound;
+    }
 
-internal class Dog(string name, string breed) : Pet(name, "Dog", "Lie down", "Woff")
-{
-    public string Breed { get; set; } = breed;
-}
+    internal class Dog(string name, string breed) : Pet(name, "Dog", "Lie down", "Woff")
+    {
+        public string Breed { get; set; } = breed;
+    }
 
-internal class Cat(string name, string breed) : Pet(name, "Cat", "Cuddle", "Purr")
-{
-    public string Breed { get; set; } = breed;
-}
+    internal class Cat(string name, string breed) : Pet(name, "Cat", "Cuddle", "Purr")
+    {
+        public string Breed { get; set; } = breed;
+    }
 
-internal class Fish(string name) : Pet(name, "Fish", "Splash", "Gulp");
+    internal class Fish(string name) : Pet(name, "Fish", "Splash", "Gulp");
+}
